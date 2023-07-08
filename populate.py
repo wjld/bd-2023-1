65bf5b269167f4d3c83d75c4af9d26200daf8f08
wjld <98484634+wjld@users.noreply.github.com>
2023-07-08 17:53:17 -0300

    add optional pre-populate script for disciplinas

--- populate.py
+++ populate.py
@@ -25,5 +25,23 @@ for semester in ['2022-1','2022-2','2023-1']:
                 for arg in e.args:
                     if not ('UNIQUE' in arg or 'FOREIGN KEY' in arg):
                         raise e
+    with open(f'{folder}disciplinas_{semester}.csv',encoding='utf8') as file:
+        for row in csv.reader(file):
+            if row[0] == 'cod':
+                continue
+            codigo: str = row[0].replace('/','').replace('-','')
+            nome: str = row[1]
+            codigoDepartamento: str = row[2].rjust(4,'0')
+            if (not validate.disCodigo(codigo) or not validate.disNome(nome)
+                or not validate.depCodigo(codigoDepartamento)):
+                continue
+            try:
+                cursor.execute('''insert into disciplina (codigo,nome,
+                                  FK_departamento_codigo) values (?,?,?)''',
+                                  [codigo,nome,codigoDepartamento])
+            except sqlite3.IntegrityError as e:
+                for arg in e.args:
+                    if not ('UNIQUE' in arg or 'FOREIGN KEY' in arg):
+                        raise e
 db.commit()
 cursor.close()
