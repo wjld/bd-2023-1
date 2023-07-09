5d2cc6fb4fd8edd57ddd0c2c58a8e75c9e8ec982
wjld <98484634+wjld@users.noreply.github.com>
2023-07-09 17:48:50 -0300

    add base window class for window management

---
+++ window.py
@@ -0,0 +1,44 @@
+from tkinter import Tk, ttk
+from dbConnect import DbConnect
+
+class Window:
+
+    def __init__(self):
+        self.window: Tk = Tk()
+        self.size = None
+        self.proportionalSize: tuple[int, int] = (0,0)
+        self.style: ttk.Style = ttk.Style(self.window)
+        self.height: int = self.window.winfo_screenheight()
+        self.connection = DbConnect()
+        self.window.title("Sistema de Avaliação")
+        self.window.rowconfigure(0,weight=1)
+        self.window.columnconfigure(0,weight=1)
+        self.window.bind("<Configure>",self.manageSize)
+
+        self.window.minsize(3*self.height//4, 9*self.height//16)
+        self.proportionalSize = self.window.minsize(None)
+
+    def manageSize(self,event):
+        if isinstance(event.widget,Tk) and self.isResizing(event):
+            self.size = (event.width,event.height)
+            aspectRatio = 3/4
+            maxY = int(event.width/aspectRatio)
+
+            if maxY < event.height:
+                self.proportionalSize = (event.width,maxY)
+            else:
+                self.proportionalSize = self.size
+
+            self.styleConfig(self.window,self.style,*self.proportionalSize)
+
+    def styleConfig(self, w: Tk, s: ttk.Style, x: int, y: int) -> None:
+        s.configure("title.TLabel",anchor="center",font=("Roboto",
+                    int(-y*0.085)))
+        s.configure("options.TButton",font=("Roboto",int(-y*0.04)))
+
+    def isResizing(self,event):
+        return (event.width,event.height) != self.size
+    
+    def destroy(self):
+        self.connection.close()
+        self.window.destroy()
