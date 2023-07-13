e081593721d939221077d770eeed35f9564cd47c
wjld <98484634+wjld@users.noreply.github.com>
2023-07-13 00:09:57 -0300

    refactor decrease spacing

--- dbConnect.py
+++ dbConnect.py
@@ -4,39 +4,37 @@ class DbConnect:
 
     def __init__(self) -> None:
         self.db = sqlite3.connect('./ratings.db',)
-        self.cursor = self.db.cursor()
-        self.cursor.execute('pragma foreign_keys = on')
-        self.cursor.execute('pragma case_sensitive_like=off')
+        cursor = self.db.cursor()
+        self.ex = cursor.execute
+        self.fa = cursor.fetchall
+        self.cl = cursor.close
+        self.ex('pragma foreign_keys = on')
+        self.ex('pragma case_sensitive_like=off')
         self.views()
     
     def views(self):
-        self.cursor.execute('drop view if exists search')
-        self.cursor.execute('''create view search as
-                               select d.codigo as disciplina_codigo,
-                               d.nome as disciplina_nome,
-                               pt.FK_turma_numero as turma_numero,
-                               p.nom_prim_nome || ' ' || p.nom_sobrenome as
-                               professor_nome,
-                               pt.FK_turma_semestre as turma_semestre,
-                               p.matricula as matricula
-                               from disciplina d inner join professor_turma pt
-                               on d.codigo = pt.FK_disciplina_codigo
-                               inner join professor p
-                               on pt.fk_professor_matricula = p.matricula''')
+        self.ex('drop view if exists search')
+        self.ex('''create view search as select d.codigo as disciplina_codigo,
+                   d.nome as disciplina_nome,pt.FK_turma_numeroas turma_numero,
+                   p.nom_prim_nome || ' ' || p.nom_sobrenome as professor_nome,
+                   pt.FK_turma_semestre as turma_semestre,
+                   p.matricula as matricula from disciplina d inner join
+                   professor_turma pt on d.codigo = pt.FK_disciplina_codigo
+                   inner join professor p
+                   on pt.fk_professor_matricula = p.matricula''')
 
     def close(self) -> None:
         self.db.commit()
-        self.cursor.close()
+        self.cl()
 
     def getSemester(self):
-        self.cursor.execute('''select semestre from turma 
-                               group by semestre order by semestre desc''')
-        return [x[0] for x in self.cursor.fetchall()]
+        self.ex('''select semestre from turma group by semestre
+                   order by semestre desc''')
+        return [x[0] for x in self.fa()]
 
     def avg(self,order: str,type: str,semester: str) -> list[tuple[str,str]]:
         if type == 'Turmas':
-            self.cursor.execute(
-                f'''select avg(a.nota) as media,d.codigo,d.nome,
+            self.ex(f'''select avg(a.nota) as media,d.codigo,d.nome,
                            pt.FK_turma_numero
                     from avaliacao a inner join professor_turma pt
                     on (pt.FK_professor_matricula = a.FK_professor_matricula
@@ -49,30 +47,25 @@ class DbConnect:
                     pt.FK_turma_numero,pt.FK_turma_semestre,d.nome
                     order by media {order} limit 100;''')
         elif type == 'Professores':
-            self.cursor.execute(f'''select avg(a.nota) as media,
-                                    p.nom_prim_nome,p.nom_sobrenome
-                                    from professor p inner join avaliacao a
-                                    on p.matricula = a.FK_professor_matricula
-                                    and a.FK_turma_semestre = "{semester}"
-                                    group by p.matricula
-                                    order by media {order} limit 100;''')
-        return self.cursor.fetchall()[::-1]
+            self.ex(f'''select avg(a.nota) as media,p.nom_prim_nome,
+                        p.nom_sobrenome from professor p inner join avaliacao a
+                        on p.matricula = a.FK_professor_matricula
+                        and a.FK_turma_semestre = "{semester}"
+                        group by p.matricula
+                        order by media {order} limit 100;''')
+        return self.fa()[::-1]
 
     def hasAdmins(self):
-        self.cursor.execute('''select count(*) from usuario
-                               where administrador = 1''')
-        return self.cursor.fetchall()[0][0]
+        self.ex('''select count(*) from usuario where administrador = 1''')
+        return self.fa()[0][0]
 
     def signin(self,username,password):
-        self.cursor.execute(f'''select * from usuario
-                                where (matricula = "{username}"
-                                or email = "{username}")
-                                and senha = "{password}"''')
-        return self.cursor.fetchall()
+        self.ex(f'''select * from usuario where (matricula = "{username}"
+                    or email = "{username}") and senha = "{password}"''')
+        return self.fa()
 
     def search(self,s,semester):
-        self.cursor.execute(
-            f'''select * from search
+        self.ex(f'''select * from search
                 where (disciplina_nome like "%{s}%"
                       or disciplina_codigo like "%{s}%"
                       or professor_nome like "%{s}%")
@@ -82,47 +75,41 @@ class DbConnect:
                 coalesce(nullif(instr(lower(disciplina_codigo),"{s}"),0),9999),
                 coalesce(nullif(instr(lower(professor_nome),"{s}"),0),9999))
                 limit 50;''')
-        return self.cursor.fetchall()[::-1] if s else None
+        return self.fa()[::-1] if s else None
     
     def viewOwn(self,matricula,semester):
-        self.cursor.execute(f'''select a.FK_disciplina_codigo,d.nome,
-                                a.FK_turma_numero,
-                                p.nom_prim_nome || ' ' || p.nom_sobrenome,
-                                a.FK_turma_semestre,p.matricula,a.nota,
-                                a.texto from avaliacao a
-                                inner join professor p 
-                                on p.matricula = a.FK_professor_matricula
-                                inner join disciplina d 
-                                on d.codigo = a.FK_disciplina_codigo
-                                where a.FK_usuario_matricula =
-                                      "{matricula}"
-                                and a.FK_turma_semestre = {semester}''')
-        return self.cursor.fetchall()[::-1]
+        self.ex(f'''select a.FK_disciplina_codigo,d.nome,a.FK_turma_numero,
+                    p.nom_prim_nome || ' ' || p.nom_sobrenome,
+                    a.FK_turma_semestre,p.matricula,a.nota,
+                    a.texto from avaliacao a inner join professor p 
+                    on p.matricula = a.FK_professor_matricula
+                    inner join disciplina d
+                    on d.codigo = a.FK_disciplina_codigo
+                    where a.FK_usuario_matricula = "{matricula}"
+                    and a.FK_turma_semestre = {semester}''')
+        return self.fa()[::-1]
 
     def rated(self,matricula,info):
-        self.cursor.execute(f'''select exists (select * from avaliacao a 
-                                where a.FK_usuario_matricula = "{matricula}"
-                                and FK_professor_matricula = "{info[5]}"
-                                and FK_disciplina_codigo = "{info[0]}"
-                                and FK_turma_numero = "{info[2]}"
-                                and FK_turma_semestre = "{info[4]}")''')
-        return bool(self.cursor.fetchall()[0][0])
+        self.ex(f'''select exists (select * from avaliacao a 
+                    where a.FK_usuario_matricula = "{matricula}"
+                    and FK_professor_matricula = "{info[5]}"
+                    and FK_disciplina_codigo = "{info[0]}"
+                    and FK_turma_numero = "{info[2]}"
+                    and FK_turma_semestre = "{info[4]}")''')
+        return bool(self.fa()[0][0])
     
     def updateRating(self,matricula,info,grade,text):
-        self.cursor.execute(f'''update avaliacao set texto = ?,nota = ? where
-                                FK_usuario_matricula = ?
-                                and FK_professor_matricula = ?
-                                and FK_disciplina_codigo = ?
-                                and FK_turma_numero = ?
-                                and FK_turma_semestre = ?''',
-                                [text,grade,matricula,info[5],info[0],info[2],
-                                 info[4]])
+        self.ex(f'''update avaliacao set texto = ?,nota = ?
+                    where FK_usuario_matricula = ?
+                    and FK_professor_matricula = ? and FK_disciplina_codigo = ?
+                    and FK_turma_numero = ? and FK_turma_semestre = ?''',
+                    [text,grade,matricula,info[5],info[0],info[2],
+                        info[4]])
     
     def recordRating(self,matricula,info,grade,text):
-        self.cursor.execute(f'''insert into avaliacao(texto,nota,
-                              FK_usuario_matricula,FK_professor_matricula,
-                              FK_disciplina_codigo,FK_turma_numero,
-                              FK_turma_semestre)
-                              values (?,?,?,?,?,?,?)''',
-                              [text,grade,matricula,info[5],info[0],info[2],
-                               info[4]])
+        self.ex(f'''insert into avaliacao(texto,nota,
+                    FK_usuario_matricula,FK_professor_matricula,
+                    FK_disciplina_codigo,FK_turma_numero,
+                    FK_turma_semestre) values (?,?,?,?,?,?,?)''',
+                    [text,grade,matricula,info[5],info[0],info[2],
+                    info[4]])
